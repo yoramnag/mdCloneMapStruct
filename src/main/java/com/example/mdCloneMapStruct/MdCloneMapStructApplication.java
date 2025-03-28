d59bf8610c1f8998b1fa5915542b25f710a1db24
yoram <yoramnag@gmail.com>
2025-03-29 00:29:23 +0300

    adding new files for the project

---
+++ src/main/java/com/example/mdCloneMapStruct/MdCloneMapStructApplication.java
@@ -0,0 +1,13 @@
+package com.example.mdCloneMapStruct;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MdCloneMapStructApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MdCloneMapStructApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/mdCloneMapStruct/MdCloneMapStructApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mdCloneMapStruct;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MdCloneMapStructApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
